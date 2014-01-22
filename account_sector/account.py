d4b26e39601a7bd75feb4c3c5e8678a8be33d47d
miescuderol <miescuderol@gmail.com>
2014-01-22 11:41:18 -0300

    se agregaron las clases AccountTemplate y SectorTemplate al módulo accout_sector, tira error de tipo en el xml cuando verifica el el campo sector de las cuetnas

--- account_sector/account.py
+++ account_sector/account.py
@@ -1,15 +1,31 @@
 #-*- coding: utf-8 -*-
 from trytond.model import ModelSQL, ModelView, fields
 
-__all__ = ['Account', 'Sector']
+__all__ = ['Account', 'Sector', 'AccountTemplate', 'SectorTemplate']
 
-class Account(ModelSQL, ModelView):
-    "Account"
-    __name__ = 'account.account'
-    sector_id = fields.Many2One('account_sector.sector', 'Sector', required=False)
+
+class SectorTemplate(ModelSQL, ModelView):
+    "Sector Template"
+    __name__ = 'account_sector.sector.template'
+    name = fields.Char('Nombre', required=True)
+    cuentas = fields.One2Many('account.account.template', 'sector_id', 'Cuentas', required=False)
 
 class Sector(ModelSQL, ModelView):
     "Sector"
     __name__ = 'account_sector.sector'
     name = fields.Char('Nombre', required=True)
     cuentas = fields.One2Many('account.account', 'sector_id', 'Cuentas', required=False)
+    template = fields.Many2One('account_sector.sector.template', 'Template')
+
+
+class AccountTemplate(ModelSQL, ModelView):
+    'Account Template'
+    __name__ = 'account.account.template'
+    sector_id = fields.Many2One('account_sector.sector.template', 'Sector', ondelete="RESTRICT", required=False)
+
+
+class Account(ModelSQL, ModelView):
+    "Account"
+    __name__ = 'account.account'
+    sector_id = fields.Many2One('account_sector.sector', 'Sector', ondelete="RESTRICT", required=False)
+
