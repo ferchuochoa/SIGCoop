64f57a439b973d8609ce4ccb8b6e2dfe5240e6c8
zikofv <zikofv@gmail.com>
2014-01-24 10:12:25 -0300

    Primer version de suministro

--- sigcoop_usuario/__init__.py
+++ sigcoop_usuario/__init__.py
@@ -1,11 +1,13 @@
 from trytond.pool import Pool
 from .party import *
 from .familiar import Familiar
+from .suministro import Suministro
 
 
 def register():
     Pool.register(
         Party,
+        Suministro,
         Familiar,
         module='sigcoop_usuario', type_='model'
     )
